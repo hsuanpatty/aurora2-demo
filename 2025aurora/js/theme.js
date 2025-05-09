be87bd9dd6f170f08c0dc1df7f36a5be9a3866c6
hsuanpatty <106578679+hsuanpatty@users.noreply.github.com>
2025-05-09 16:50:33 +0800

    Add files via upload

---
+++ 2025aurora/js/theme.js
@@ -0,0 +1,48 @@
+
+$(".owl-carousel1").owlCarousel({
+		loop: true,
+	dots: true,
+	margin: 10,
+	autoplay: true,
+	responsive: {
+		0: {
+			items: 1
+		},
+		767: {
+			items: 1
+		},
+	}
+})
+
+
+$(".owl-carousel2").owlCarousel({
+	loop: true,
+	dots: true,
+	margin: 10,
+	autoplay: true,
+	responsive: {
+		0: {
+			items: 2
+		},
+		576: {
+			items: 2
+		},
+	}
+})
+
+
+
+$(".owl-carousel3").owlCarousel({
+	loop: true,
+	dots: true,
+	margin: 10,
+	autoplay: true,
+	responsive: {
+		0: {
+			items: 1
+		},
+		767: {
+			items: 1
+		},
+	}
+})
